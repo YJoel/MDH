2fdedbbb93d85d63352888b88922e664a1decc7f
YJoel <joelgarcia6601@gmail.com>
2024-01-05 16:55:49 -0500

    arreglo de la pagina de noticias

--- pages/noticias/index.php
+++ pages/noticias/index.php
@@ -1,31 +1,91 @@
-<div class='pageTitle pb-4'>
-    <h1> NOTICIAS </h1>
-</div>
-<div class="noticiasContainer p-5">
-    <div class="row p-3">
-        <div class="noticiasDestacadas">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita facilis, quaerat delectus eius aperiam
-            deserunt qui fugit voluptas! Aspernatur vitae dolor aperiam molestiae veniam unde distinctio ea molestias
-            necessitatibus atque!
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-4">
-            <div class="noticia">
+<?php
 
+$pageName = "NOTICIAS";
+
+?>
+
+<html lang="es">
+
+<head>
+  <meta charset="utf-8" />
+  <title>
+    MDH -
+    <?php echo $pageName; ?>
+  </title>
+  <link rel="stylesheet" href="../../css/styles.css" />
+  <link rel="shortcut icon" href="../../images/simbolo_azul1.png" type="image/x-icon" />
+
+  <script src="../../scripts/script.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <meta name="keywords" content="Libertad, Religiosa, Iglesia" />
+  <meta name="description"
+    content="La Fundación Mi derecho es un Hecho es una Organización sin ánimo de lucro que vela por los derechos humanos. Su propósito básico consiste en la construcción, desarrollo y fortalecimiento de la sociedad, formando líderes que sean capaces de desarrollar autonomía, Influencia, Transformación y construcción de una generación de conquista" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+  <?php
+
+  include_once "../../resources/nav-bar.html";
+  include_once "../../resources/animacion-circulos.html";
+
+  ?>
+  <div class="container">
+    <div class="content">
+      <div class='pageTitle pb-4'>
+        <h1> NOTICIAS </h1>
+      </div>
+      <!--
+        <div class="noticiasContainer p-5">
+          <div class="row p-3">
+            <div class="noticiasDestacadas">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita facilis, quaerat delectus
+              eius aperiam
+              deserunt qui fugit voluptas! Aspernatur vitae dolor aperiam molestiae veniam unde distinctio ea
+              molestias
+              necessitatibus atque!
             </div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt soluta, dolore quam dignissimos eos quisquam
-            et debitis, iusto sint voluptatibus aliquid sequi molestiae minus rerum porro similique cupiditate aut
-            error.
+          </div>
+          <div class="row">
+            <div class="col-4">
+              <div class="noticia">
+
+              </div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt soluta, dolore quam dignissimos
+              eos quisquam
+              et debitis, iusto sint voluptatibus aliquid sequi molestiae minus rerum porro similique
+              cupiditate aut
+              error.
+            </div>
+            <div class="col-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eligendi harum natus quo delectus
+              tempore
+              dolore quaerat ut optio sed animi a dolorum odio, fuga culpa asperiores nulla reprehenderit
+              aliquid?
+            </div>
+            <div class="col-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum et error esse quisquam earum?
+              Voluptate
+              ipsa possimus harum temporibus facere excepturi, non ratione aperiam, tempora inventore at nobis
+              recusandae
+              suscipit.
+            </div>
+          </div>
         </div>
-        <div class="col-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia eligendi harum natus quo delectus tempore
-            dolore quaerat ut optio sed animi a dolorum odio, fuga culpa asperiores nulla reprehenderit aliquid?
-        </div>
-        <div class="col-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum et error esse quisquam earum? Voluptate
-            ipsa possimus harum temporibus facere excepturi, non ratione aperiam, tempora inventore at nobis recusandae
-            suscipit.
-        </div>
+      -->
     </div>
-</div>
+  </div>
+
+  <?php
+
+  include_once "../../resources/footer.html";
+
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
